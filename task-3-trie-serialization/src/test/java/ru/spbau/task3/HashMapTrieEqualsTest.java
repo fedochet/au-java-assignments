d16f0fe16a957598773c14152c8db133bc60652b
Roman Golyshev <anstreth@gmail.com>
2018-04-06 16:55:19 +0300

    add more tests for equality

--- task-3-trie-serialization/src/test/java/ru/spbau/task3/HashMapTrieEqualsTest.java
+++ task-3-trie-serialization/src/test/java/ru/spbau/task3/HashMapTrieEqualsTest.java
@@ -2,6 +2,9 @@ package ru.spbau.task3;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class HashMapTrieEqualsTest {
@@ -23,6 +26,72 @@ class HashMapTrieEqualsTest {
     void empty_tries_are_equal() {
         HashMapTrie trie1 = new HashMapTrie();
         HashMapTrie trie2 = new HashMapTrie();
+
+        assertThat(trie1).isEqualTo(trie2);
+        assertThat(trie2).isEqualTo(trie1);
+    }
+
+    @Test
+    void tries_with_same_content_are_equal() {
+        List<String> words = Arrays.asList("hello", "world", "one", "two", "three");
+        HashMapTrie trie1 = new HashMapTrie();
+        HashMapTrie trie2 = new HashMapTrie();
+
+        words.forEach(word -> {
+            trie1.add(word);
+            trie2.add(word);
+        });
+
+        assertThat(trie1).isEqualTo(trie2);
+        assertThat(trie2).isEqualTo(trie1);
+    }
+
+    @Test
+    void tries_with_one_different_word_are_not_equal() {
+        List<String> words1 = Arrays.asList("hello", "world", "one", "two", "three");
+        List<String> words2 = Arrays.asList("hello", "world", "one", "two", "three", "four");
+        HashMapTrie trie1 = new HashMapTrie();
+        HashMapTrie trie2 = new HashMapTrie();
+
+        words1.forEach(trie1::add);
+        words2.forEach(trie2::add);
+
+        assertThat(trie1).isNotEqualTo(trie2);
+        assertThat(trie2).isNotEqualTo(trie1);
+    }
+
+    @Test
+    void tries_with_different_words_are_not_equal() {
+        List<String> words1 = Arrays.asList("one", "two", "three");
+        List<String> words2 = Arrays.asList("four", "five", "six");
+        HashMapTrie trie1 = new HashMapTrie();
+        HashMapTrie trie2 = new HashMapTrie();
+
+        words1.forEach(trie1::add);
+        words2.forEach(trie2::add);
+
+        assertThat(trie1).isNotEqualTo(trie2);
+        assertThat(trie2).isNotEqualTo(trie1);
+    }
+
+    @Test
+    void equal_tries_are_not_equal_after_removing_and_equal_after_addition() {
+        List<String> words = Arrays.asList("one", "two", "three");
+        HashMapTrie trie1 = new HashMapTrie();
+        HashMapTrie trie2 = new HashMapTrie();
+
+        words.forEach(trie1::add);
+        words.forEach(trie2::add);
+
+        assertThat(trie1).isEqualTo(trie2);
+        assertThat(trie2).isEqualTo(trie1);
+
+        trie1.add("four");
+
+        assertThat(trie1).isNotEqualTo(trie2);
+        assertThat(trie2).isNotEqualTo(trie1);
+
+        trie2.add("four");
 
         assertThat(trie1).isEqualTo(trie2);
         assertThat(trie2).isEqualTo(trie1);
