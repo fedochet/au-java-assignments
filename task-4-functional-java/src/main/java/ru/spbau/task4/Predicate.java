d00ffe56b4a2f7bef0993054fa8cdd1e84d1b255
Roman Golyshev <anstreth@gmail.com>
2018-04-13 17:29:20 +0300

    add AND and OR functions to `Predicate`

--- task-4-functional-java/src/main/java/ru/spbau/task4/Predicate.java
+++ task-4-functional-java/src/main/java/ru/spbau/task4/Predicate.java
@@ -1,5 +1,23 @@
 package ru.spbau.task4;
 
 public abstract class Predicate<T> extends Function1<T, Boolean> {
+    Predicate<T> or(Predicate<T> other) {
+        Predicate<T> that = this;
+        return new Predicate<T>() {
+            @Override
+            public Boolean apply(T arg) {
+                return that.apply(arg) || other.apply(arg);
+            }
+        };
+    }
 
+    Predicate<T> and(Predicate<T> other) {
+        Predicate<T> that = this;
+        return new Predicate<T>() {
+            @Override
+            public Boolean apply(T arg) {
+                return that.apply(arg) && other.apply(arg);
+            }
+        };
+    }
 }
--- task-4-functional-java/src/test/java/ru/spbau/task4/PredicateTest.java
+++ task-4-functional-java/src/test/java/ru/spbau/task4/PredicateTest.java
@@ -23,4 +23,44 @@ class PredicateTest {
 
         assertThat(isEvenFunc.apply(10)).isTrue();
     }
+
+    @Test
+    void OR_works_for_predicates() {
+        Predicate<String> isFoo = createPredicate(s -> s.equals("foo"));
+        Predicate<String> isBoo = createPredicate(s -> s.equals("boo"));
+
+        Predicate<String> fooOrBoo = isFoo.or(isBoo);
+        Predicate<String> booOrFoo = isBoo.or(isFoo);
+
+        assertThat(fooOrBoo.apply("foo")).isTrue();
+        assertThat(booOrFoo.apply("foo")).isTrue();
+
+        assertThat(fooOrBoo.apply("boo")).isTrue();
+        assertThat(booOrFoo.apply("boo")).isTrue();
+
+        assertThat(fooOrBoo.apply("other")).isFalse();
+        assertThat(booOrFoo.apply("other")).isFalse();
+    }
+
+    @Test
+    void AND_works_for_predicates() {
+        Predicate<String> startsWithFoo = createPredicate(s -> s.startsWith("foo"));
+        Predicate<String> endsWithBoo = createPredicate(s -> s.endsWith("boo"));
+
+        Predicate<String> fooSomethingBoo = startsWithFoo.and(endsWithBoo);
+        Predicate<String> fooSomethingBooRev = endsWithBoo.and(startsWithFoo);
+
+        assertThat(fooSomethingBoo.apply("fooboo")).isTrue();
+        assertThat(fooSomethingBooRev.apply("fooboo")).isTrue();
+
+        assertThat(fooSomethingBoo.apply("boo")).isFalse();
+        assertThat(fooSomethingBooRev.apply("boo")).isFalse();
+
+        assertThat(fooSomethingBoo.apply("foo")).isFalse();
+        assertThat(fooSomethingBooRev.apply("foo")).isFalse();
+
+        assertThat(fooSomethingBoo.apply("other")).isFalse();
+        assertThat(fooSomethingBooRev.apply("other")).isFalse();
+
+    }
 }
