b1661f4039df912f299a5e47d336462087422b9a
Roman Golyshev <anstreth@gmail.com>
2018-04-13 18:23:10 +0300

    make Function1 types more flexible

--- task-4-functional-java/src/main/java/ru/spbau/task4/Function1.java
+++ task-4-functional-java/src/main/java/ru/spbau/task4/Function1.java
@@ -1,12 +1,12 @@
 package ru.spbau.task4;
 
-public abstract class Function1<T, M> {
-    public abstract M apply(T arg);
+public abstract class Function1<T, R> {
+    public abstract R apply(T arg);
 
-    public final <A> Function1<T, A> compose(Function1<M, A> length) {
-        return new Function1<T, A>() {
+    public final <V> Function1<T, V> compose(Function1<? super R, ? extends V> length) {
+        return new Function1<T, V>() {
             @Override
-            public A apply(T arg) {
+            public V apply(T arg) {
                 return length.apply(Function1.this.apply(arg));
             }
         };
--- task-4-functional-java/src/test/java/ru/spbau/task4/Function1Test.java
+++ task-4-functional-java/src/test/java/ru/spbau/task4/Function1Test.java
@@ -1,9 +1,10 @@
 package ru.spbau.task4;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
-import static ru.spbau.task4.FunctionUtils.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.spbau.task4.FunctionUtils.createFunction1;
 
 class Function1Test {
 
@@ -24,4 +25,26 @@ class Function1Test {
         assertThat(lengthToString.apply("hello world")).isEqualTo("11");
     }
 
+    @Test
+    @DisplayName("composition works with function that takes wider argument")
+    void composition_works_for_wider_argument_type() {
+        Function1<Object, Boolean> notNull = createFunction1(o -> o != null);
+        Function1<String, String> stringId = createFunction1(s -> s);
+
+        Function1<String, Boolean> notNullString = stringId.compose(notNull);
+
+        assertThat(notNullString.apply("str")).isTrue();
+        assertThat(notNullString.apply(null)).isFalse();
+    }
+
+    @Test
+    @DisplayName("composition works with function that returns more narrow type")
+    void composition_works_for_narrower_return_type() {
+        Function1<Object, Object> id = createFunction1(i -> i);
+        Function1<Object, String> toString = createFunction1(i -> i.toString());
+
+        Function1<Object, Object> toStringObject = id.compose(toString);
+
+        assertThat(toStringObject.apply(100)).isEqualTo("100");
+    }
 }
