03a0a3d6f0aff5a4bb2b095131591e254e86f585
Roman Golyshev <anstreth@gmail.com>
2018-04-14 05:43:09 +0300

    refactor `Collections`

--- task-4-functional-java/src/main/java/ru/spbau/task4/Collections.java
+++ task-4-functional-java/src/main/java/ru/spbau/task4/Collections.java
@@ -8,6 +8,18 @@ public final class Collections {
     private Collections() {
     }
 
+    public static <T, U> U foldr(Iterable<? extends T> iterable, Function2<? super T, ? super U, ? extends U> f, U zero) {
+        return recursiveFoldr(iterable.iterator(), f, zero);
+    }
+
+    /**
+     * See <a href='https://wiki.haskell.org/Foldl_as_foldr'>https://wiki.haskell.org/Foldl_as_foldr</a>.
+     */
+    public static <T, U> U foldl(Iterable<? extends T> iterable, Function2<? super U, ? super T, ? extends U> f, U zero) {
+        Function1<U, U> foldlWithFoldr = foldr(iterable, (b, g) -> (x) -> g.apply(f.apply(x, b)), x -> x);
+        return foldlWithFoldr.apply(zero);
+    }
+
     public static <T, R> Iterable<R> map(Iterable<? extends T> iterable, Function1<? super T, ? extends R> mapper) {
         return foldl(
             iterable,
@@ -34,18 +46,6 @@ public final class Collections {
 
     public static <T> Iterable<T> takeUnless(Iterable<? extends T> iterable, Predicate<? super T> condition) {
         return takeWhile(iterable, condition.not());
-    }
-
-    public static <T, U> U foldr(Iterable<? extends T> iterable, Function2<? super T, ? super U, ? extends U> f, U zero) {
-        return recursiveFoldr(iterable.iterator(), f, zero);
-    }
-
-    /**
-     * See <a href='https://wiki.haskell.org/Foldl_as_foldr'>https://wiki.haskell.org/Foldl_as_foldr</a>.
-     */
-    public static <T, U> U foldl(Iterable<? extends T> iterable, Function2<? super U, ? super T, ? extends U> f, U zero) {
-        Function1<U, U> foldlWithFoldr = foldr(iterable, (b, g) -> (x) -> g.apply(f.apply(x, b)), x -> x);
-        return foldlWithFoldr.apply(zero);
     }
 
     private static <U, T> U recursiveFoldr(Iterator<? extends T> iterator, Function2<? super T, ? super U, ? extends U> f, U zero) {
