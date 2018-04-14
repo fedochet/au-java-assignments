1ab843c23b7557cf35cee9e987d87b7fc661a4af
Roman Golyshev <anstreth@gmail.com>
2018-04-14 04:51:13 +0300

    implement foldl with foldr

--- task-4-functional-java/src/main/java/ru/spbau/task4/Collections.java
+++ task-4-functional-java/src/main/java/ru/spbau/task4/Collections.java
@@ -23,20 +23,19 @@ public final class Collections {
         );
     }
 
-    public static <T, U> U foldr(Iterable<T> iterable, Function2<T, U, U> f, U zero) {
+    public static <T, U> U foldr(Iterable<? extends T> iterable, Function2<? super T, ? super U, ? extends U> f, U zero) {
         return recursiveFoldr(iterable.iterator(), f, zero);
     }
 
+    /**
+     * See <a href='https://wiki.haskell.org/Foldl_as_foldr'>https://wiki.haskell.org/Foldl_as_foldr</a>.
+     */
     public static <T, U> U foldl(Iterable<? extends T> iterable, Function2<? super U, ? super T, ? extends U> f, U zero) {
-        U result = zero;
-        for (T t : iterable) {
-            result = f.apply(result, t);
-        }
-
-        return result;
+        Function1<U, U> foldlWithFoldr = foldr(iterable, (b, g) -> (x) -> g.apply(f.apply(x, b)), x -> x);
+        return foldlWithFoldr.apply(zero);
     }
 
-    private static <U, T> U recursiveFoldr(Iterator<T> iterator, Function2<T, U, U> f, U zero) {
+    private static <U, T> U recursiveFoldr(Iterator<? extends T> iterator, Function2<? super T, ? super U, ? extends U> f, U zero) {
         if (iterator.hasNext()) {
             T current = iterator.next();
             U rest = recursiveFoldr(iterator, f, zero);
