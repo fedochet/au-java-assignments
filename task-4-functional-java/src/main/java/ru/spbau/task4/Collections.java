bd2af0ea37b6551a29e78c57009189bcf75e9645
Roman Golyshev <anstreth@gmail.com>
2018-04-14 05:43:09 +0300

    refactor `Collections`

--- task-4-functional-java/src/main/java/ru/spbau/task4/Collections.java
+++ task-4-functional-java/src/main/java/ru/spbau/task4/Collections.java
@@ -3,9 +3,22 @@ package ru.spbau.task4;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 public final class Collections {
     private Collections() {
+    }
+
+    public static <T, U> U foldr(Iterable<? extends T> iterable, Function2<? super T, ? super U, ? extends U> f, U zero) {
+        return recursiveFoldr(iterable.iterator(), f, zero);
+    }
+
+    /**
+     * See <a href='https://wiki.haskell.org/Foldl_as_foldr'>https://wiki.haskell.org/Foldl_as_foldr</a>.
+     */
+    public static <T, U> U foldl(Iterable<? extends T> iterable, Function2<? super U, ? super T, ? extends U> f, U zero) {
+        Function1<U, U> foldlWithFoldr = foldr(iterable, (b, g) -> (x) -> g.apply(f.apply(x, b)), x -> x);
+        return foldlWithFoldr.apply(zero);
     }
 
     public static <T, R> Iterable<R> map(Iterable<? extends T> iterable, Function1<? super T, ? extends R> mapper) {
@@ -36,18 +49,6 @@ public final class Collections {
         return takeWhile(iterable, condition.not());
     }
 
-    public static <T, U> U foldr(Iterable<? extends T> iterable, Function2<? super T, ? super U, ? extends U> f, U zero) {
-        return recursiveFoldr(iterable.iterator(), f, zero);
-    }
-
-    /**
-     * See <a href='https://wiki.haskell.org/Foldl_as_foldr'>https://wiki.haskell.org/Foldl_as_foldr</a>.
-     */
-    public static <T, U> U foldl(Iterable<? extends T> iterable, Function2<? super U, ? super T, ? extends U> f, U zero) {
-        Function1<U, U> foldlWithFoldr = foldr(iterable, (b, g) -> (x) -> g.apply(f.apply(x, b)), x -> x);
-        return foldlWithFoldr.apply(zero);
-    }
-
     private static <U, T> U recursiveFoldr(Iterator<? extends T> iterator, Function2<? super T, ? super U, ? extends U> f, U zero) {
         if (iterator.hasNext()) {
             T current = iterator.next();
@@ -58,12 +59,12 @@ public final class Collections {
         return zero;
     }
 
-    private static <R> ArrayList<R> pushBack(ArrayList<R> acc, R elem) {
+    private static <E, L extends List<? super E>> L pushBack(L acc, E elem) {
         acc.add(elem);
         return acc;
     }
 
-    private static <R> LinkedList<R> pushFront(LinkedList<R> acc, R elem) {
+    private static <E, L extends List<? super E>> L pushFront(L acc, E elem) {
         acc.add(0, elem);
         return acc;
     }
