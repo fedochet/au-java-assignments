828c7cd3591657e380c9be6709c023d32faa5331
Roman Golyshev <anstreth@gmail.com>
2018-05-11 15:06:57 +0300

    add task 5 with partial solution

---
+++ task-5-stream-api/src/main/java/ru/spbau/mit/FirstPartTasks.java
@@ -0,0 +1,101 @@
+package ru.spbau.mit;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public final class FirstPartTasks {
+
+    private FirstPartTasks() {}
+
+    // Список названий альбомов
+    public static List<String> allNames(Stream<Album> albums) {
+        return albums.map(Album::getName).collect(Collectors.toList());
+    }
+
+    // Список названий альбомов, отсортированный лексикографически по названию
+    public static List<String> allNamesSorted(Stream<Album> albums) {
+        return albums.map(Album::getName)
+            .sorted()
+            .collect(Collectors.toList());
+    }
+
+    // Список треков, отсортированный лексикографически по названию, включающий все треки альбомов из 'albums'
+    public static List<String> allTracksSorted(Stream<Album> albums) {
+        return albums.flatMap(album -> album.getTracks().stream())
+            .map(Track::getName)
+            .sorted()
+            .collect(Collectors.toList());
+    }
+
+    // Список альбомов, в которых есть хотя бы один трек с рейтингом более 95, отсортированный по названию
+    public static List<Album> sortedFavorites(Stream<Album> albums) {
+        Predicate<Album> hasPopularSong = album -> album.getTracks().stream().anyMatch(track -> track.getRating() > 95);
+
+        return albums.filter(hasPopularSong)
+            .sorted(Comparator.comparing(Album::getName))
+            .collect(Collectors.toList());
+    }
+
+    // Сгруппировать альбомы по артистам
+    public static Map<Artist, List<Album>> groupByArtist(Stream<Album> albums) {
+        return albums.collect(Collectors.groupingBy(Album::getArtist));
+    }
+
+    // Сгруппировать альбомы по артистам (в качестве значения вместо объекта 'Artist' использовать его имя)
+    public static Map<Artist, List<String>> groupByArtistMapName(Stream<Album> albums) {
+        return albums.collect(Collectors.groupingBy(
+            Album::getArtist,
+            Collectors.mapping(Album::getName, Collectors.toList())
+        ));
+    }
+
+    // Число повторяющихся альбомов в потоке
+    public static long countAlbumDuplicates(Stream<Album> albums) {
+        return albums.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+            .values().stream()
+            .mapToLong(count -> count - 1)
+            .sum();
+    }
+
+    // Альбом, в котором максимум рейтинга минимален
+    // (если в альбоме нет ни одного трека, считать, что максимум рейтинга в нем --- 0)
+    public static Optional<Album> minMaxRating(Stream<Album> albums) {
+        return albums.min(Comparator.comparing(album -> getTrackWithBestRating(album).map(Track::getRating).orElse(0)));
+    }
+
+    private static Optional<Track> getTrackWithBestRating(Album a) {
+        return a.getTracks().stream().max(Comparator.comparing(Track::getRating));
+    }
+
+    // Список альбомов, отсортированный по убыванию среднего рейтинга его треков (0, если треков нет)
+    public static List<Album> sortByAverageRating(Stream<Album> albums) {
+        return albums
+            .sorted(Comparator.comparing((Album album) -> getAverageTrackRating(album).orElse(0)).reversed())
+            .collect(Collectors.toList());
+    }
+
+    private static OptionalDouble getAverageTrackRating(Album album) {
+        return album.getTracks().stream().mapToInt(Track::getRating).average();
+    }
+
+    // Произведение всех чисел потока по модулю 'modulo'
+    // (все числа от 0 до 10000)
+    public static int moduloProduction(IntStream stream, int modulo) {
+        return stream.reduce(1, (acc, a) -> (acc * a) % modulo);
+    }
+
+    // Вернуть строку, состояющую из конкатенаций переданного массива, и окруженную строками "<", ">"
+    // см. тесты
+    public static String joinTo(String... strings) {
+        return Arrays.stream(strings).collect(Collectors.joining(", ", "<", ">"));
+    }
+
+    // Вернуть поток из объектов класса 'clazz'
+    public static <R> Stream<R> filterIsInstance(Stream<?> s, Class<R> clazz) {
+        return s.filter(clazz::isInstance).map(clazz::cast);
+    }
+}
