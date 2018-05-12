cffc32d69ac78715af927fb7eb5bf535006d598f
Roman Golyshev <anstreth@gmail.com>
2018-05-12 15:58:09 +0300

    add solutions and tests for second part of task
    - no test for quotes search

--- task-5-stream-api/src/test/java/ru/spbau/mit/SecondPartTasksTest.java
+++ task-5-stream-api/src/test/java/ru/spbau/mit/SecondPartTasksTest.java
@@ -1,7 +1,13 @@
 package ru.spbau.mit;
 
+import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class SecondPartTasksTest {
@@ -13,16 +19,52 @@ public class SecondPartTasksTest {
 
     @Test
     public void testPiDividedBy4() {
-        fail();
+        double areaOfCircle = Math.PI * Math.pow(0.5, 2);
+
+        assertEquals(areaOfCircle, SecondPartTasks.piDividedBy4(), 0.01);
     }
 
     @Test
     public void testFindPrinter() {
-        fail();
+        Map<String, List<String>> authorsWithBooks = ImmutableMap.<String, List<String>>builder()
+            .put("aaa", Arrays.asList("abc", "def", "g", "hi")) // 9
+            .put("bbb", Arrays.asList("ab", "de", "f"))         // 5
+            .put("ccc", Arrays.asList("abcd", "efgh"))          // 8
+            .build();
+
+        assertEquals("aaa", SecondPartTasks.findPrinter(authorsWithBooks).get());
     }
 
     @Test
     public void testCalculateGlobalOrder() {
-        fail();
+        Map<String, Integer> map1 = ImmutableMap.<String, Integer>builder()
+            .put("aaa", 1)
+            .put("bbb", 2)
+            .put("ccc", 3)
+            .build();
+
+        Map<String, Integer> map2 = ImmutableMap.<String, Integer>builder()
+            .put("aaa", 4)
+            .put("bbb", 5)
+            .put("ddd", 6)
+            .build();
+
+        Map<String, Integer> map3 = ImmutableMap.<String, Integer>builder()
+            .put("eee", 10)
+            .put("fff", 11)
+            .put("ggg", 12)
+            .build();
+
+        ImmutableMap<String, Integer> expected = ImmutableMap.<String, Integer>builder()
+            .put("aaa", 5)
+            .put("bbb", 7)
+            .put("ccc", 3)
+            .put("ddd", 6)
+            .put("eee", 10)
+            .put("fff", 11)
+            .put("ggg", 12)
+            .build();
+
+        assertEquals(expected, SecondPartTasks.calculateGlobalOrder(Arrays.asList(map1, map2, map3)));
     }
 }
