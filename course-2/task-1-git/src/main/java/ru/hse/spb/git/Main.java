f38d03734ba640811a20a4ef570d056921c2e17a
Roman Golyshev <anstreth@gmail.com>
2018-09-09 17:51:07 +0300

    milestone-1: add first draft of classes for git

--- course-2/task-1-git/src/main/java/ru/hse/spb/git/Main.java
+++ course-2/task-1-git/src/main/java/ru/hse/spb/git/Main.java
@@ -1,5 +1,50 @@
 package ru.hse.spb.git;
 
+import lombok.Data;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+
+enum ObjectType {
+    BLOB, FILE_TREE
+}
+
+@Data
+final class ObjectRef {
+    private final String hash;
+    private final ObjectType type;
+}
+
+@Data
+final class Blob {
+    private final String hash;
+    private final Path file;
+}
+
+@Data
+final class FileTree {
+    private final String hash;
+    private final Path file;
+    private final String name;
+    private final List<ObjectRef> content; // hashes
+}
+
+@Data
+final class Commit {
+    private final String hash;
+    private final Path file;
+    private final String message;
+    private final List<ObjectRef> content; // hashes
+}
+
+interface Repository {
+    String getHead();
+    Optional<Blob> getBlob(String hash);
+    Optional<FileTree> getFileTree(String hash);
+    Optional<Commit> getCommit(String hash);
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
