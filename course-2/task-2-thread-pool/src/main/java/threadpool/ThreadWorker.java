20a63f556f906e8b9fc0933c5189bd8f700ee019
Roman Golyshev <anstreth@gmail.com>
2018-11-15 22:19:36 +0300

    simplify `ThreadWorker`

--- course-2/task-2-thread-pool/src/main/java/threadpool/ThreadWorker.java
+++ course-2/task-2-thread-pool/src/main/java/threadpool/ThreadWorker.java
@@ -10,34 +10,23 @@ final class ThreadWorker implements Runnable {
 
     @Override
     public void run() {
-        Task task;
         while (!isStopped) {
             try {
-                task = queue.remove();
-            } catch (InterruptedException e) {
-                // if it is an actual stop, we will see `isStopped` set to true and will stop `while`,
-                // because interrupting thread HB seeing interruption
-                // if it is a spurious wake up, we will try again
-                continue;
+                Task task = queue.remove();
+                task.run();
+            } catch (InterruptedException ignored) {
+                // If it is an actual stop, we will see `isStopped` set to true and will stop `while`,
+                // because interrupting thread HB seeing interruption.
+                //
+                // This exception may raise in run() only when task is awaiting for other future to complete. So, if it
+                // is thrown, then executor thread has been interrupted, and threadPool is shutting down. That's why
+                // we can afford to lose task, taken from queue.
             }
-
-            executeTask(task);
         }
     }
 
-    public void stop() {
+    void stop() {
         isStopped = true;
     }
 
-    private void executeTask(Task task) {
-        while (!isStopped) {
-            try {
-
-                task.run();
-                return;
-
-            } catch (InterruptedException ignored) {
-            }
-        }
-    }
 }
