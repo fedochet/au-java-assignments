d5f21e35f709f43327f29e13b04134e93aacc2ff
Roman Golyshev <anstreth@gmail.com>
2018-11-28 16:04:16 +0300

    task-3-torrent: add initial projects structure

---
+++ course-2/task-3-torrent/torrent-client/src/test/java/org/anstreth/torrent/client/MainTest.java
@@ -0,0 +1,13 @@
+package org.anstreth.torrent.client;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void test() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
